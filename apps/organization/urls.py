f7c9f7932914e1705dd9b4b085985ca2b94924d9
mimota1994 <mimota1994@users.noreply.github.com>
2018-03-26 20:38:46 +0800

    small impovement

---
+++ apps/organization/urls.py
@@ -0,0 +1,10 @@
+#_*_encoding:utf-8_*_
+
+from django.conf.urls import url,include
+
+from .views import OrgView,AddUserAskView
+
+urlpatterns=[
+    url(r'^list/$',OrgView.as_view(),name="org_list"),
+    url(r'^add_ask/$',AddUserAskView.as_view(),name="add_ask"),
+]
